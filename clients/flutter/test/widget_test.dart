41f8040a01f9940d7044a88d8f95ea0403fe8eca
Marc Yefimchuk <marc.yefimchuk@gmail.com>
2024-08-08 17:04:16 +0200

    fix: Fix tests

--- clients/flutter/test/widget_test.dart
+++ clients/flutter/test/widget_test.dart
@@ -7,24 +7,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:camera/camera.dart';
 
 import 'package:flutter_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    WidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final cameras = await availableCameras();
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(TakePictureScreen(camera: cameras.first));
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.camera));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
